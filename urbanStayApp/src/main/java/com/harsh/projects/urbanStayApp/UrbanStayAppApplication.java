55e44d163031beb87d0c59487cedb8c8f5df9c0d
hashcodez2004 <kumarsharmaharsh2004@gmail.com>
2026-04-08 14:30:23 +0530

    Initial commit

---
+++ urbanStayApp/src/main/java/com/harsh/projects/urbanStayApp/UrbanStayAppApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.projects.urbanStayApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanStayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanStayAppApplication.class, args);
+	}
+
+}
---
+++ urbanStayApp/src/test/java/com/harsh/projects/urbanStayApp/UrbanStayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.projects.urbanStayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanStayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
