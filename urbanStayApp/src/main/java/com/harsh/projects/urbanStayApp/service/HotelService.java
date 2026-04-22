39122d20e0a294baf4997b6578267534c4616847
hashcodez2004 <kumarsharmaharsh2004@gmail.com>
2026-04-22 16:17:29 +0530

    Added two new endpoints in Hotel Api (updateHotelById & deleteHotelById)

--- urbanStayApp/src/main/java/com/harsh/projects/urbanStayApp/service/HotelService.java
+++ urbanStayApp/src/main/java/com/harsh/projects/urbanStayApp/service/HotelService.java
@@ -6,5 +6,9 @@ public interface HotelService {
 
     HotelDto createNewHotel(HotelDto hotelDto);
 
-    HotelDto getHotelById(Long Id);
+    HotelDto getHotelById(Long id);
+
+    HotelDto updateHotelById(Long id, HotelDto hotelDto);
+
+    void deleteHotelById(Long id);
 }
